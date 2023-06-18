2e5f990e82793b3ffaa9f481b81cafacbcc44072
amalbabu1 <amalbabuporuthur@gmail.com>
2023-06-18 21:28:25 +0530

    sequlize connection added

--- src/config/config.js
+++ src/config/config.js
@@ -9,5 +9,12 @@ module.exports ={
     port:process.env.PORT,
     jwt:{
         secret:process.env.JWT_SECRET
-    }  
+    },
+    db:{
+        host:process.env.DB_HOST,
+        port:process.env.DB_PORT,
+        database:process.env.DB_DATABASE,
+        username:process.env.DB_USERNAME,
+        password:process.env.DB_PASSWORD
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const app =  require('./app')
 const config = require('./config/config')
+const sequelize = require('./database/config/config')
+
 
 app.listen(config.port,()=>{
   console.log( `Listening on PORT ${config.port}`)
 })
+
